793b1ba97b4cb0f5f166f072f82e2f5f8c6f25b2
Amah David <amahdavid34@gmail.com>
2024-10-16 18:01:08 -0700

    Implemented styling for payment and cart, removed some components

--- Ecommerce_Application/Pages/Index.cshtml.cs
+++ Ecommerce_Application/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 using Ecommerce_Application.Data;
 using Ecommerce_Application.Models;
+using Ecommerce_Application.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,20 +11,26 @@ namespace Ecommerce_Application.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly AppDbContext _context;
+        private readonly CartService _cartService;
 
-        public IndexModel(ILogger<IndexModel> logger, AppDbContext context)
+        public IndexModel(ILogger<IndexModel> logger, AppDbContext context, CartService cartService)
         {
             _logger = logger;
             _context = context;
-
+            _cartService = cartService;
         }
 
         public List<Product> Products { get; set; }
 
-
         public async Task OnGetAsync()
         {
             Products = await _context.Products.ToListAsync();
+        }
+
+        public IActionResult OnPostAddToCart(Guid productId)
+        {
+            _cartService.AddToCart(productId);
+            return RedirectToPage();
         }
     }
 }
